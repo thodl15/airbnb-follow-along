74045e49bceefba2981f1ace0bc9088514aaf457
David Thomas <thodl15@gmail.com>
2018-10-30 17:02:16 -0400

    Created the register view component and referenced it in the connector.

---
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import Button from "antd/lib/button";
+
+export class RegisterView extends React.PureComponent {
+    render() {
+        return <Button type="primary">Button</Button>;
+}}
