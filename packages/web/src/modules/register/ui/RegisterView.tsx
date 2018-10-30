7432ce51e25191c2ab62fe616c59907f988a48e2
David Thomas <thodl15@gmail.com>
2018-10-30 17:15:32 -0400

    Added basic styling for registration page.

--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,7 +1,36 @@
 import * as React from 'react'
-import Button from "antd/lib/button";
+import {Form, Icon, Input, Button} from "antd";
+
+const FormItem = Form.Item;
 
 export class RegisterView extends React.PureComponent {
-    render() {
-        return <Button type="primary">Button</Button>;
-}}
+
+
+  render() {
+    return (
+        <div style={{display: 'flex'}}>
+            <div style={{width: 400, margin: 'auto'}}>
+                <FormItem>
+                	<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                </FormItem>
+                <FormItem>
+                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                </FormItem>
+                <FormItem>
+                    <a className="login-form-forgot" href="">
+                        Forgot password
+                    </a>
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                    Register
+                    </Button>
+                </FormItem>
+                <FormItem>
+                    Or <a href="">login now!</a>
+                </FormItem>
+            </div>
+        </div>
+    );
+  }
+}
