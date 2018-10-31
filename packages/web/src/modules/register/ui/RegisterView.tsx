0125221b3540cb27685a6b92099282ffa8b18a40
David Thomas <thodl15@gmail.com>
2018-10-31 12:44:29 -0400

    Added form validation on the client-side using yup.

--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import {Form, Icon, Input, Button} from "antd";
 import {withFormik, FormikErrors, FormikProps }  from 'formik';
+import * as yup from 'yup';
 
 const FormItem = Form.Item;
 
@@ -15,11 +16,17 @@ interface Props {
 
 class C extends React.PureComponent<FormikProps<FormValues> & Props> {
   render() {
-	  const { values, handleChange, handleBlur, handleSubmit } = this.props;
+	  const { values, handleChange, handleBlur, handleSubmit, touched, errors } = this.props;
     return (
         <form style={{display: 'flex'}} onSubmit={handleSubmit}>
             <div style={{width: 400, margin: 'auto'}}>
-                <FormItem>
+				<FormItem 
+					help={touched.email && errors.email ? errors.email : ""}
+					// tslint:disable-next-line:jsx-no-multiline-js
+					validateStatus={
+						touched.email && errors.email ? "error" : undefined
+					}
+				>
 					<Input
 						name="email"
 						prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} 
@@ -28,8 +35,14 @@ class C extends React.PureComponent<FormikProps<FormValues> & Props> {
 						onChange={handleChange}
 						onBlur={handleBlur}
 					/>
-                </FormItem>
-                <FormItem>
+				</FormItem>
+				<FormItem 
+					help={touched.password && errors.password ? errors.password : ""}
+					// tslint:disable-next-line:jsx-no-multiline-js
+					validateStatus={
+						touched.password && errors.password ? "error" : undefined
+					}
+				>
 					<Input
 						name="password"
 						prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} 
@@ -39,7 +52,7 @@ class C extends React.PureComponent<FormikProps<FormValues> & Props> {
 						onChange={handleChange}
 						onBlur={handleBlur}
 					/>
-                </FormItem>
+				</FormItem>
                 <FormItem>
                     <a className="login-form-forgot" href="">
                         Forgot password
@@ -62,7 +75,27 @@ class C extends React.PureComponent<FormikProps<FormValues> & Props> {
     );
   }
 }
+
+const emailNotLongEnough = "email must be at least 3 characters";
+const passwordNotLongEnough = "password must be at least 3 characters";
+const invalidEmail = "email must be a valid email";
+
+const validationSchema = yup.object().shape({
+	email: yup
+		.string()
+		.min(3, emailNotLongEnough)
+		.max(255)
+		.email(invalidEmail)
+		.required(),
+	password: yup
+		.string()
+		.min(3, passwordNotLongEnough)
+		.max(255)
+		.required()
+});
+
 export const RegisterView = withFormik<Props, FormValues>({
+	validationSchema,
 	mapPropsToValues: () => ({ email: "", password: "" }),
 	handleSubmit: async (values, {props, setErrors, setSubmitting}) => {
 		const errors = await props.submit(values);
